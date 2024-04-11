234ee8eb0c783c3ee0249e7ddf62cb1abfe05057
lus1s <luiscarlosantos1235@gmail.com>
2024-04-11 16:58:41 -0300

    foi

--- arquivos/js/script.js
+++ arquivos/js/script.js
@@ -1,18 +1,4 @@
 function validacao() {
-    alert("vacca");
-    const nomepet = document.getElementById("nomepet");
-    const raca = document.getElementById('racapet');
-    alert('oii');
+    let servico = getElementById("servico");
 
-    if (nomepet.value == "") {
-        alert("Preencha o nome do seu pet");
-        return false;
-    }
-    
-
-    if (raca.value == "") {
-        alert("Preencha o nome do seu pet");
-        return false;
-    }
-    
 }
