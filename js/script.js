a3e516516a16338e9d7972e2bd4a86a1f4647cfa
Janulha <anajuliampm2007@gmail.com>
2024-04-11 16:32:56 -0300

    teste2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 function validacao() {
+    alert("vacca");
+    const nomepet = document.getElementById("nomepet");
+    const raça = document.getElementById('raçapet').value;
+    alert('oii');
+
+    if (nomepet.value == "") {
+        alert("Preencha o nome do seu pet");
+        return false;
+    }
     
 }
